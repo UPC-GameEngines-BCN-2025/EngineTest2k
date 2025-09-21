35ec1b10038726de0897dd5467a9cc2d88bd3334
paolo2kk <gpaucayuela@gmail.com>
2025-09-21 17:04:24 +0200

    VAMONOS

---
+++ Engine/Engine.cpp
@@ -0,0 +1,16 @@
+﻿// Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Engine.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+int main()
+{
+	cout << "VAMONOS." << endl;
+	return 0;
+}
